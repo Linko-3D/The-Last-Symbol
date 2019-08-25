4a776df2ed8b443f4affc050e6409b0d494ee954
Linko-3D <dbekhouc@gmail.com>
2019-08-25 23:40:22 +0200

    tweak

--- Assets/AmmoSpawn.gd
+++ Assets/AmmoSpawn.gd
@@ -1,5 +1,13 @@
 extends Area
 
+var speed = 0.05
+
+func _process(delta):
+	randomize()
+
+	rotate_x(rand_range(-speed, speed))
+	rotate_y(rand_range(-speed, speed))
+	rotate_z(rand_range(-speed, speed))
 
 func _on_AmmoSpawn_body_entered(body):
 	if body.name == "FirstPersonCharacter":
