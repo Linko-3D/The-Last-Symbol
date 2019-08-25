72aeacf1b92e8db39e2af39cded70f70fc7e5006
Linko-3D <dbekhouc@gmail.com>
2019-08-25 17:43:11 +0200

    tweak

--- Templates/3D/Interactive/Teleporter/Teleporter.gd
+++ Templates/3D/Interactive/Teleporter/Teleporter.gd
@@ -3,6 +3,10 @@ extends Area
 export(Vector3) var destination
 #target = get_tree().get_nodes_in_group("Player")[0] # Get the target with the first node in the Player group
 
+func _process(delta):
+	$Rotor.rotate_y(0.3)
+
+
 func _on_TeleporterA_body_entered(body):
 	if body.is_in_group("Player"):
 		body.translation = destination
