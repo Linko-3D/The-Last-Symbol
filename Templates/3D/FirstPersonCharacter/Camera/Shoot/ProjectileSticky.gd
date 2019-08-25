676238a28e8d3674aa944a7f26472c0344d14b60
Linko-3D <dbekhouc@gmail.com>
2019-08-25 16:36:27 +0200

    tweak

--- Templates/3D/FirstPersonCharacter/Camera/Shoot/ProjectileSticky.gd
+++ Templates/3D/FirstPersonCharacter/Camera/Shoot/ProjectileSticky.gd
@@ -1,16 +1,29 @@
 extends RigidBody
 
 var lifespan = 10
+var target
+var target_coordinates = Vector3()
+var current_coordinates = Vector3()
+var offset = Vector3()
 
 func _ready():
-	friction = 0
-	$Timer.wait_time = lifespan
-	$Timer.start()
+	$AnimationPlayer.play("Light")
+	yield(get_tree().create_timer(lifespan), "timeout")
+	$OmniLight.queue_free()
 
-func _on_Timer_timeout():
-	queue_free()
+func _process(delta):
+	if target != null:
+		translation = target.translation + offset # Crash happens, need fix
+	else:
+		axis_lock_linear_z = false
+		axis_lock_linear_x = false
+		axis_lock_linear_y = false
 
 func _on_Area_body_entered(body):
-	axis_lock_linear_x = true
-	axis_lock_linear_y = true
-	axis_lock_linear_z = true
+	if body.is_in_group("Player") == false:
+		target = body
+		target_coordinates = body.translation
+		offset = translation - target_coordinates
+		axis_lock_linear_z = true
+		axis_lock_linear_x = true
+		axis_lock_linear_y = true
