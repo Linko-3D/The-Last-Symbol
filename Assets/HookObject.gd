59c98dbe96c3161b6daa343b0016b730a765c311
Linko-3D <dbekhouc@gmail.com>
2019-09-14 22:53:55 +0200

    tweak

--- Assets/HookObject.gd
+++ Assets/HookObject.gd
@@ -5,9 +5,6 @@ var target
 func _ready():
 	target = get_tree().get_nodes_in_group("Player")[0] # Get the first node in the group "Player"
 
-func _process(delta):
-	print(target.can_hook)
-	
 func _on_HookObject_body_entered(body):
 	if body == target:
 		target.can_hook = true
